dd82174dee0bb8dc99c268992f5922dfb0498a1f
jm <jean-michel.caron@jmitechnologies.com>
2020-10-08 22:12:23 -0400

    update

--- src/TraductionServiceProvider.php
+++ src/TraductionServiceProvider.php
@@ -15,31 +15,8 @@ class TraductionServiceProvider extends ServiceProvider
         /*
          * Optional methods to load your package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'traduction');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'traduction');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
+        
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('traduction.php'),
-            ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/traduction'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/traduction'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/traduction'),
-            ], 'lang');*/
-
             // Registering package commands.
             $this->commands([
                 TraductionMissing::class
@@ -52,9 +29,6 @@ class TraductionServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'traduction');
-
         // Register the main class to use with the facade
         $this->app->singleton('traduction', function () {
             return new Traduction;
